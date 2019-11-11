bba373a5265de0979d997f9ee34cd41cb3cc021d
Alejandro <alejandro.ruizdiaz91@gmail.com>
2019-11-10 23:29:34 -0300

    Ejemplo de sprint data rest con swagger

---
+++ src/test/java/alerd/tutorial/datarest/DatarestApplicationTests.java
@@ -0,0 +1,13 @@
+package alerd.tutorial.datarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
